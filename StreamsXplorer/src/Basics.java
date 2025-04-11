61659e8609e9d01a0b585ccc06ff14d336f32768
Vignesh S <vigneshkavitha2001@gmail.com>
2025-04-11 10:00:23 +0530

    JAVA - Streams Exercises

--- StreamsXplorer/src/Basics.java
+++ StreamsXplorer/src/Basics.java
@@ -9,7 +9,21 @@ public class Basics {
         // printALlNumberFunctionalPrograming(numbers);
         // printEvenNumberStructuralPrograming(numbers);
         // printEvenNumberFunctionalPrograming(numbers);
-    }
+
+        /// Task 1 - print only the odd numbers
+        System.out.println("****** TASK 1 ******");
+        numbers.stream().filter(n -> n%2!=0).forEach(System.out::println);
+
+        /// Task 2 - print Courses containing "Spring" only
+        System.out.println("****** TASK 2 ******");
+        List<String> courses = List.of("Spring", "Spring boot", "API", "Docker", "JPA", "Database", "AWS");
+        courses.stream().filter(c -> c.contains("Spring")).forEach(System.out::println);
+
+        /// Task 3 - print Courses whose name has at least 4 letters
+        System.out.println("****** TASK 3 ******");
+        courses.stream().filter(c->c.length()>4).forEach(System.out::println);
+
+        }
 
     // Structural Programming - Take each numbers in the list and prints it
     public static void printALlNumberStructuralPrograming(List<Integer> numbers) {
@@ -35,11 +49,10 @@ public class Basics {
     public static void printALlNumberFunctionalPrograming(List<Integer> numbers){
 
         // numbers.stream().forEach(Basics::display);
-        numbers.stream().forEach(System.out::println);
+        numbers.forEach(System.out::println);
 
         // System.out::println, Basics::display --> Method References
     }
-
 
     // Functional programming filters - To filter the stream and process it
     public static void printEvenNumberFunctionalPrograming(List<Integer> numbers) {
@@ -48,4 +61,5 @@ public class Basics {
                 // Lambda Expression - with one statement it will return that statement's output otherwise we need to provide return statement.
                 .forEach(System.out::println);
     }
+
 }
