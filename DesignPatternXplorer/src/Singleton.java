1f844f12404c06ceb91d2deddd6b38e72287b575
Vignesh S <vigneshkavitha2001@gmail.com>
2025-04-15 23:03:24 +0530

    JAVA - Design Pattern Singleton

--- DesignPatternXplorer/src/Singleton.java
+++ DesignPatternXplorer/src/Singleton.java
@@ -34,3 +34,45 @@ class SingletonStaticBlock {
         return singletonStaticBlock;
     }
 }
+
+/// Singleton Lazy
+class SingletonLazy {
+    private static SingletonLazy singletonLazy = null;
+
+    private SingletonLazy() {}
+
+    public static SingletonLazy getInstance(){
+        if (singletonLazy == null){
+            singletonLazy = new SingletonLazy();
+        }
+        return singletonLazy;
+    }
+}
+
+/// Singleton Thread Safe
+class SingletonThreadSafe {
+    private static SingletonThreadSafe singletonThreadSafe = null;
+
+    private SingletonThreadSafe() {}
+
+    public static SingletonThreadSafe getInstance(){
+        synchronized (singletonThreadSafe){singletonThreadSafe = new SingletonThreadSafe();}
+        return singletonThreadSafe;
+    }
+}
+
+/// Singleton Double Check Lock
+class SingletonDCLock {
+    private static SingletonDCLock singletonDCLock = null;
+
+    private SingletonDCLock() {}
+
+    public static SingletonDCLock getInstance(){
+        synchronized (singletonDCLock){
+            if (singletonDCLock == null) {
+                singletonDCLock = new SingletonDCLock();
+            }
+        }
+        return singletonDCLock;
+    }
+}
