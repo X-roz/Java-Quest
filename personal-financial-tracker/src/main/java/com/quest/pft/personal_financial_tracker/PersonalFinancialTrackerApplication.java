256b7a72b7cca0d9390d3688b27789de16f93fc7
Vignesh S <vigneshkavitha2001@gmail.com>
2025-05-15 22:47:46 +0530

    Project: Personal Financial Tracker

---
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/PersonalFinancialTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.quest.pft.personal_financial_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialTrackerApplication.class, args);
+	}
+
+}
---
+++ personal-financial-tracker/src/test/java/com/quest/pft/personal_financial_tracker/PersonalFinancialTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest.pft.personal_financial_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancialTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
