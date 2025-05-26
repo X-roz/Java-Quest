ca0f8b19b9136eb8b8fd173ecc6853a57b94eea6
Vignesh S <vigneshkavitha2001@gmail.com>
2025-05-26 09:02:38 +0530

    Finance Tracker: Enum object

---
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/model/enums/AccountType.java
@@ -0,0 +1,5 @@
+package com.quest.pft.personal_financial_tracker.model.enums;
+
+public enum AccountType {
+    BASIC, PREMIUM;
+}
---
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/model/enums/Category.java
@@ -0,0 +1,5 @@
+package com.quest.pft.personal_financial_tracker.model.enums;
+
+public enum Category {
+    FOOD, INVESTMENT, CREDIT, ENTERTAINMENT, RENT, TRAVEL;
+}
