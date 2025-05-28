535bea5738bde32e0ce58b4a99e83ece364438cb
Vignesh S <vigneshkavitha2001@gmail.com>
2025-05-28 22:38:28 +0530

    Finance Tracker: User DTO added

--- personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/controller/UserController.java
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/controller/UserController.java
@@ -1,17 +1,17 @@
 package com.quest.pft.personal_financial_tracker.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.quest.pft.personal_financial_tracker.dto.UserDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @PostMapping("create")
-    private void createUser() {
-
+    private void createUser(@RequestBody UserDto userDto) {
+        log.info("Creating new user : {}", userDto);
     }
 
     @GetMapping("login")
