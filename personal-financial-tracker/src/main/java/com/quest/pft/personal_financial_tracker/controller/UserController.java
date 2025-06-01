3581f366f065c9babab518a8a17f2a72cd622143
Vignesh S <vigneshkavitha2001@gmail.com>
2025-06-01 23:54:11 +0530

    Finance Tracker: Create user endpoint

--- personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/controller/UserController.java
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.quest.pft.personal_financial_tracker.controller;
 
+import com.quest.pft.personal_financial_tracker.config.PasswordConfig;
 import com.quest.pft.personal_financial_tracker.dto.UserDto;
+import com.quest.pft.personal_financial_tracker.model.User;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Log4j2
 @RestController
@@ -12,6 +16,14 @@ public class UserController {
     @PostMapping("create")
     private void createUser(@RequestBody UserDto userDto) {
         log.info("Creating new user : {}", userDto);
+        User user = new User();
+        user.setId(String.valueOf(UUID.randomUUID()));
+        user.setIncome(userDto.getIncome());
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+//        user.setEncryptedPassword();
+//        user.setAccountType();
     }
 
     @GetMapping("login")
--- personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/dto/UserDto.java
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/dto/UserDto.java
@@ -1,12 +1,14 @@
 package com.quest.pft.personal_financial_tracker.dto;
 
 import com.quest.pft.personal_financial_tracker.model.enums.AccountType;
+import lombok.Data;
 
+@Data
 public class UserDto {
 
     private String name;
     private String password;
-    private AccountType accountType;
+    private String accountType;
     private String email;
     private String phoneNumber;
     private float income;
