710cb4582025cfade02e9ed0dc42f2995ea84ecf
Vignesh S <vigneshkavitha2001@gmail.com>
2025-06-08 21:53:26 +0530

    Finance Tracker: Add Users in cache

--- personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/service/UserService.java
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/service/UserService.java
@@ -1,5 +1,6 @@
 package com.quest.pft.personal_financial_tracker.service;
 
+import com.quest.pft.personal_financial_tracker.cache.UserCacheContainer;
 import com.quest.pft.personal_financial_tracker.dto.UserDto;
 import com.quest.pft.personal_financial_tracker.model.User;
 import lombok.extern.log4j.Log4j2;
@@ -11,6 +12,8 @@ import java.util.UUID;
 @Service
 public class UserService {
 
+    private UserCacheContainer userCacheContainer;
+
     public void CreateUser(UserDto userDto){
         log.info("Creating new user : {}", userDto);
         User user = new User();
@@ -19,6 +22,7 @@ public class UserService {
         user.setEmail(userDto.getEmail());
         user.setName(userDto.getName());
         user.setPhoneNumber(userDto.getPhoneNumber());
+        userCacheContainer.addUserData(user);
         log.info("User Created! userId = {}", user.getId());
     }
 }
