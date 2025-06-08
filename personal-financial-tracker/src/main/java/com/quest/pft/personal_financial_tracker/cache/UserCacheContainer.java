96a81ee295b0ec366836539f469c1ef7f02ea108
Vignesh S <vigneshkavitha2001@gmail.com>
2025-06-08 21:49:11 +0530

    Finance Tracker: User added in cache

--- personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/cache/UserCacheContainer.java
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/cache/UserCacheContainer.java
@@ -1,4 +1,15 @@
 package com.quest.pft.personal_financial_tracker.cache;
 
+import com.quest.pft.personal_financial_tracker.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserCacheContainer {
+
+    private final Map<String, User> userContainer = new HashMap<>();
+
+    public void addUserData(User user) {
+        userContainer.putIfAbsent(user.getId(), user);
+    }
 }
