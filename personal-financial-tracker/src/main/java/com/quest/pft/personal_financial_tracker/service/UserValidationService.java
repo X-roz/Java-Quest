368b1a2c526ad8b8cbd8aed390ce13c1815c526d
Vignesh S <vigneshkavitha2001@gmail.com>
2025-06-15 19:19:03 +0530

    Finance Tracker: User Validation service

---
+++ personal-financial-tracker/src/main/java/com/quest/pft/personal_financial_tracker/service/UserValidationService.java
@@ -0,0 +1,11 @@
+package com.quest.pft.personal_financial_tracker.service;
+
+import com.quest.pft.personal_financial_tracker.model.User;
+
+public class UserValidationService {
+
+    public boolean createUserValidation(User user) {
+        return false;
+    }
+
+}
