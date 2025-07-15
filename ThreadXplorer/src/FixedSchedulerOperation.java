c2f4d20c154e6b2df822e9ed7c0c6a7a22cf7ec6
Vignesh S <vigneshkavitha2001@gmail.com>
2025-07-15 22:42:41 +0530

    Fixed Scheduler Operation

--- ThreadXplorer/src/FixedSchedulerOperation.java
+++ ThreadXplorer/src/FixedSchedulerOperation.java
@@ -1,2 +1,31 @@
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class FixedSchedulerOperation {
+
+    public static void main(String[] args){
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        AtomicInteger count = new AtomicInteger();
+        Runnable display = () -> {
+            int currentCount = count.incrementAndGet();
+            System.out.println("Current Time : " + LocalTime.now());
+            if(currentCount > 10){
+                System.out.println("Scheduler stopped after 10 executions.");
+                executorService.shutdown();
+            }
+        };
+
+        executorService.scheduleWithFixedDelay(display, 0, 10, TimeUnit.SECONDS);
+
+        try {
+            executorService.awaitTermination(120, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
