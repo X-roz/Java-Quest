d67fe0023f1e939308fce3243381c65ac8d99262
Vignesh S <vigneshkavitha2001@gmail.com>
2025-07-24 23:03:47 +0530

    Linked List Solid

---
+++ DsaXplorer/src/linked-list/LLNode.java
@@ -0,0 +1,13 @@
+public class LLNode {
+    String val;
+    LLNode nextNode;
+    private int size;
+
+    public void addNode() {
+
+    }
+
+    public int size() {
+        return size;
+    }
+}
